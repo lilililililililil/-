f50e995ee13a239620d96aa6781916543f165ed4
李旺 <1025319993@qq.com>
2016-12-01 16:15:25 +0800

    工程

---
+++ 自学武术/自学武术/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自学武术
+//
+//  Created by 李旺 on 2016/12/1.
+//  Copyright © 2016年 自学武术项目组. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
