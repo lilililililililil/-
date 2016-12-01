b59ed9dfa09d4d7db1e1d15d2a4f13cd04d9db1b
李旺 <1025319993@qq.com>
2016-12-01 17:15:11 +0800

    pch

---
+++ 自学武术/自学武术/类/CustomScrollView.m
@@ -0,0 +1,26 @@
+//
+//  CustomScrollView.m
+//  自学武术
+//
+//  Created by 李旺 on 2016/12/1.
+//  Copyright © 2016年 自学武术项目组. All rights reserved.
+//
+
+#import "CustomScrollView.h"
+
+@implementation CustomScrollView
+-(instancetype)initWithFrame:(CGRect)frame{
+    if(self==[super initWithFrame:frame]){
+        
+    }
+    return self;
+}
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
